a07c79425d706cdd2ef4c22e339e24492cbe6cfe
rezvanr2378@gmail.com <rezvanr2378@gmail.com>
2022-07-13 19:43:39 +0200

    Added DAP, very dapper

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,24 +9,26 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'                                                    -- Package manager
-  use 'tpope/vim-fugitive'                                                        -- Git commands in nvim
-  use 'tpope/vim-rhubarb'                                                         -- Fugitive-companion to interact with github
-  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } }       -- Add git related info in the signs columns and popups
-  use 'numToStr/Comment.nvim'                                                     -- "gc" to comment visual regions/lines
-  use 'nvim-treesitter/nvim-treesitter'                                           -- Highlight, edit, and navigate code
-  use 'nvim-treesitter/nvim-treesitter-textobjects'                               -- Additional textobjects for treesitter
-  use 'neovim/nvim-lspconfig'                                                     -- Collection of configurations for built-in LSP client
-  use 'williamboman/nvim-lsp-installer'                                           -- Automatically install language servers to stdpath
-  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } }               -- Autocompletion
-  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } }           -- Snippet Engine and Snippet Expansion
-  use 'folke/tokyonight.nvim'                                                     -- Theme inspired by Atom
-  use 'nvim-lualine/lualine.nvim'                                                 -- Fancier statusline
-  use 'lukas-reineke/indent-blankline.nvim'                                       -- Add indentation guides even on blank lines
-  use 'tpope/vim-sleuth'                                                          -- Detect tabstop and shiftwidth automatically
+  use 'wbthomason/packer.nvim' -- Package manager
+  use 'tpope/vim-fugitive' -- Git commands in nvim
+  use 'tpope/vim-rhubarb' -- Fugitive-companion to interact with github
+  use { 'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Add git related info in the signs columns and popups
+  use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
+  use 'nvim-treesitter/nvim-treesitter' -- Highlight, edit, and navigate code
+  use 'nvim-treesitter/nvim-treesitter-textobjects' -- Additional textobjects for treesitter
+  use 'neovim/nvim-lspconfig' -- Collection of configurations for built-in LSP client
+  use 'williamboman/nvim-lsp-installer' -- Automatically install language servers to stdpath
+  use { 'hrsh7th/nvim-cmp', requires = { 'hrsh7th/cmp-nvim-lsp' } } -- Autocompletion
+  use { 'L3MON4D3/LuaSnip', requires = { 'saadparwaiz1/cmp_luasnip' } } -- Snippet Engine and Snippet Expansion
+  use 'folke/tokyonight.nvim' -- Theme inspired by Atom
+  use 'nvim-lualine/lualine.nvim' -- Fancier statusline
+  use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
+  use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
   use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } } -- Fuzzy Finder (files, lsp, etc)
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
-
+  use 'mfussenegger/nvim-dap'
+  use { "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } }
+  use 'theHamsta/nvim-dap-virtual-text'
   if is_bootstrap then
     require('packer').sync()
   end
@@ -39,14 +41,17 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = vim.fn.expand '$MYVIMRC',
 })
 
+
 -- Settings
+
+
 -- Splits
 vim.o.splitbelow = true
 vim.o.splitright = true
 
 vim.o.lazyredraw = true
-vim.o.ruler   = true
-vim.o.showcmd = true
+vim.o.ruler      = true
+vim.o.showcmd    = true
 
 -- Set highlight on search
 vim.o.hlsearch = false
@@ -73,11 +78,14 @@ vim.o.updatetime = 250
 vim.wo.signcolumn = 'yes'
 
 -- Set colorscheme
-vim.o.termguicolors               = true
-vim.g.tokyonight_style            = "night"
-vim.g.tokyonight_sidebars         = { "qf", "vista_kind", "terminal", "packer" }
-vim.g.tokyonight_colors           = { hint = "orange", error = "#ff0000" }
+vim.o.termguicolors       = true
+vim.g.tokyonight_style    = "night"
+vim.g.tokyonight_sidebars = { "qf", "vista_kind", "terminal", "packer" }
+vim.g.tokyonight_colors   = { hint = "orange", error = "#ff0000" }
 vim.cmd [[colorscheme tokyonight]]
+--vim.api.nvim_command('hi Normal guibg=NONE')
+
+vim.api.nvim_command('set clipboard+=unnamedplus')
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
@@ -87,27 +95,30 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Movement
-vim.keymap.set('n', '<C-Left>' , 'b' , { silent = true })
-vim.keymap.set('n', '<C-Right>', 'w' , { silent = true })
-vim.keymap.set('n', '<C-Up>'   , '5k', { silent = true })
-vim.keymap.set('n', '<C-Down>' , '5j', { silent = true })
+vim.keymap.set('n', '<C-Left>', 'b', { silent = true })
+vim.keymap.set('n', '<C-Right>', 'w', { silent = true })
+vim.keymap.set('n', '<C-Up>', '5k', { silent = true })
+vim.keymap.set('n', '<C-Down>', '5j', { silent = true })
 
 -- Movement
-vim.keymap.set('n', '<C-h>', 'b' , { silent = true })
-vim.keymap.set('n', '<C-l>', 'w' , { silent = true })
+vim.keymap.set('n', '<C-h>', 'b', { silent = true })
+vim.keymap.set('n', '<C-l>', 'w', { silent = true })
 vim.keymap.set('n', '<C-k>', '5k', { silent = true })
 vim.keymap.set('n', '<C-j>', '5j', { silent = true })
 
 -- QoL
-vim.keymap.set('i', '<C-BS>', '<C-w>'            , { silent = true })
-vim.keymap.set('n', '<C-s>' , ':w<cr>:Format<cr>', { silent = true })
-vim.keymap.set('n', '<S-a>' , ':tabp<cr>'        , { silent = true })
-vim.keymap.set('n', '<S-d>' , ':tabn<cr>'        , { silent = true })
-vim.keymap.set('n', '<tab>'  , '>>' , { silent = true })
-vim.keymap.set('n', '<S-tab>', '<<' , { silent = true })
-vim.keymap.set('v', '<tab>'  , '>gv', { silent = true })
+vim.keymap.set('i', '<C-BS>', '<C-w>', { silent = true })
+vim.keymap.set('n', '<C-s>', ':Format<cr>:w<cr>', { silent = true })
+vim.keymap.set('n', '<S-a>', ':tabp<cr>', { silent = true })
+vim.keymap.set('n', '<S-d>', ':tabn<cr>', { silent = true })
+vim.keymap.set('n', '<tab>', '>>', { silent = true })
+vim.keymap.set('n', '<S-tab>', '<<', { silent = true })
+vim.keymap.set('v', '<tab>', '>gv', { silent = true })
 vim.keymap.set('v', '<S-tab>', '<gv', { silent = true })
 vim.keymap.set('n', '<C-z>', 'u', { silent = true })
+vim.keymap.set('v', '<C-x>', 'd', { silent = true })
+vim.keymap.set('n', '<C-v>', 'p', { silent = true })
+vim.keymap.set('v', '<leader>c', 'y', { silent = true })
 
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
@@ -334,3 +345,77 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
+
+-- Debugging
+
+vim.keymap.set("n", "<F5>", ":lua require'dap'.continue()<CR>")
+vim.keymap.set("n", "<F10>", ":lua require'dap'.step_over()<CR>")
+vim.keymap.set("n", "<F11>", ":lua require'dap'.step_into()<CR>")
+vim.keymap.set("n", "<F12>", ":lua require'dap'.step_out()<CR>")
+vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
+vim.keymap.set("n", "<leader>B", ":lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>")
+vim.keymap.set("n", "<leader>lp", ":lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<CR>")
+vim.keymap.set("n", "<leader>dr", ":lua require'dap'.repl.open()<CR>")
+
+local dap = require('dap')
+dap.adapters.cppdbg = {
+  id = 'cppdbg',
+  type = 'executable',
+  command = 'C:\\Users\\Reza\\.vscode\\extensions\\ms-vscode.cpptools-1.10.8-win32-x64\\debugAdapters\\bin\\OpenDebugAD7.exe',
+  options = {
+    detached = false
+  }
+}
+
+local dap = require('dap')
+dap.configurations.cpp = {
+  {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = true,
+  },
+  {
+    name = 'Attach to gdbserver :1234',
+    type = 'cppdbg',
+    request = 'launch',
+    MIMode = 'gdb',
+    miDebuggerServerAddress = 'localhost:1234',
+    miDebuggerPath = '/usr/bin/gdb',
+    cwd = '${workspaceFolder}',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+  },
+}
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
+
+
+require("nvim-dap-virtual-text").setup {
+  enabled = true, -- enable this plugin (the default)
+  enabled_commands = true, -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+  highlight_changed_variables = true, -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+  highlight_new_as_changed = false, -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+  show_stop_reason = true, -- show stop reason when stopped for exceptions
+  commented = false, -- prefix virtual text with comment string
+  only_first_definition = true, -- only show virtual text at first definition (if there are multiple)
+  all_references = false, -- show virtual text on all all references of the variable (not only definitions)
+  filter_references_pattern = '<module', -- filter references (not definitions) pattern when all_references is activated (Lua gmatch pattern, default filters out Python modules)
+  -- experimental features:
+  virt_text_pos = 'eol', -- position of virtual text, see `:h nvim_buf_set_extmark()`
+  all_frames = false, -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+  virt_lines = false, -- show virtual lines instead of virtual text (will flicker!)
+  virt_text_win_col = nil -- position the virtual text at a fixed window column (starting from the first text column) ,
+  -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+}
+
+
+local widgets = require('dap.ui.widgets')
+local my_sidebar = widgets.sidebar(widgets.scopes)
+--my_sidebar.open()
