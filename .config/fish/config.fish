4819df96b80f4476796cd64e1cc2ca0833e4391f
rezaarezvan <rezvanr2378@gmail.com>
2023-08-15 16:57:16 +0200

    Update: Nvim and fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,14 @@ end
 
 tmux
 cd ~
+
+alias vim='nvim'
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /home/reza/miniconda3/bin/conda
+    eval /home/reza/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+set -x LD_LIBRARY_PATH /usr/lib/cuda/lib64 $LD_LIBRARY_PATH
--- .config/nvim/lua/rezvan/remap.lua
+++ .config/nvim/lua/rezvan/remap.lua
@@ -50,3 +50,4 @@ vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 -- Wrapping
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
