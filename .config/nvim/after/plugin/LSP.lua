949f92d4f10e5b75dd34c9f009661d6746a1ef91
rezaarezvan <rezvanr2378@gmail.com>
2024-04-21 11:01:08 +0200

    nvim: update lsp :]

--- .config/nvim/after/plugin/LSP.lua
+++ .config/nvim/after/plugin/LSP.lua
@@ -16,14 +16,15 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-    ensure_installed = { 'tsserver', 'rust_analyzer' },
+    ensure_installed = {
+        "pyright",
+        "clangd",
+    },
     handlers = {
-        lsp_zero.default_setup,
-        lua_ls = function()
-            local lua_opts = lsp_zero.nvim_lua_ls()
-            require('lspconfig').lua_ls.setup(lua_opts)
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
         end,
-    }
+    },
 })
 
 local cmp = require('cmp')
