7efd7a013f1ce3a06480d7e47bbcb8ccbbd30b0c
rezaarezvan <reza@rezvan.xyz>
2024-09-03 12:22:01 +0800

    zig + lazy lock

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -18,3 +18,6 @@ set -x CUDA_HOME /usr/local/cuda-12.2
 set -x PATH $CUDA_HOME/bin $PATH
 set -x LD_LIBRARY_PATH $CUDA_HOME/lib64 $LD_LIBRARY_PATH
 set PATH $PATH ~/bin
+
+# Zig
+set -x PATH /usr/local/zig-0.14.0 $PATH
