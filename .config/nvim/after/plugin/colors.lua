72e38c8c4f0892c97b8d2e31ff774858c4854396
rezaarezvan <rezvanr2378@gmail.com>
2025-02-27 20:08:37 +0100

    helix colorscheme

--- .config/nvim/after/plugin/colors.lua
+++ .config/nvim/after/plugin/colors.lua
@@ -1,14 +1,14 @@
 function ColorDeez(color)
     vim.opt.background = "dark"
-    color = color or "biscuit"
+    color = color or "helix"
     vim.cmd.colorscheme(color)
 
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSBFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalSBNC", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalSBFloat", { bg = "none" })
+    -- vim.api.nvim_set_hl(0, "NormalSBNC", { bg = "none" })
 end
 
 ColorDeez()
