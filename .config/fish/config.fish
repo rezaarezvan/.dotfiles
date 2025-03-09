a3b98c28aef5888fdf0c4341cb2a69db3f50bd06
rezaarezvan <rezvanr2378@gmail.com>
2025-03-09 19:41:32 +0100

    remove conda

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,13 +7,6 @@ cd ~
 
 alias vim='nvim'
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /home/reza/miniconda3/bin/conda
-    eval /home/reza/miniconda3/bin/conda "shell.fish" "hook" $argv | source
-end
-# <<< conda initialize <<<
-#
 set -x CUDA_HOME /usr/local/cuda-12.2
 set -x PATH $CUDA_HOME/bin $PATH
 set -x LD_LIBRARY_PATH $CUDA_HOME/lib64 $LD_LIBRARY_PATH
