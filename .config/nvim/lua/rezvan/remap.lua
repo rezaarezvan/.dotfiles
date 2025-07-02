b3ef8a694a6107be243dfdbb5f11266cf844212c
rezaarezvan <rezvanr2378@gmail.com>
2025-07-02 19:06:38 +0200

    Ex <=> Oil

--- .config/nvim/lua/rezvan/remap.lua
+++ .config/nvim/lua/rezvan/remap.lua
@@ -59,6 +59,7 @@ local function setup()
     for _, key in pairs(keys) do
         vim.keymap.set(key[1], key[2], key[3], key[4])
     end
+    vim.cmd [[command! Ex Oil]]
 end
 
 -- Export and run setup
