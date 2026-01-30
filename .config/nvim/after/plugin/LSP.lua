b956b3f158f596cec7f1250990bcfcc251f08737
rezaarezvan <rezvanr2378@gmail.com>
2026-01-30 18:18:04 +0100

    gd and others + sioyek

--- .config/nvim/after/plugin/LSP.lua
+++ .config/nvim/after/plugin/LSP.lua
@@ -15,6 +15,13 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local bufnr = event.buf
         local opts = { buffer = bufnr }
 
+        -- LSP navigation (gd is not mapped by default in 0.11+)
+        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+        vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+        vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+
         vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
 
         -- Ctrl-s: format and save
